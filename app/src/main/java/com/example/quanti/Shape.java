ba5febcd74edf8fe2437c1fc2c2f12e625e8f3e7
adnan <sudheesh@ualberta.ca>
2025-09-25 19:13:43 -0600

    Color added

--- app/src/main/java/com/example/quanti/Shape.java
+++ app/src/main/java/com/example/quanti/Shape.java
@@ -5,7 +5,7 @@ public class Shape {
     private int x;
     private int y;
 
-    private String color = "$PUT_YOUR_CHOICE_OF_COLOR";
+    private String color = "blue";
     
 
     public int getX() {
