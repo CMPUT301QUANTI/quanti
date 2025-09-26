9bed24a7d1325928da93f5a16fc8704e91a7661f
Hetang Mehta <hetangmehta03@gmail.com>
2025-09-25 19:13:42 -0600

    Upadted color

--- app/src/main/java/com/example/quanti/Shape.java
+++ app/src/main/java/com/example/quanti/Shape.java
@@ -5,8 +5,8 @@ public class Shape {
     private int x;
     private int y;
 
-    String color = "blue";
-    
+    String color = "This is not a color";
+
     public int getX() {
         return x;
     }
